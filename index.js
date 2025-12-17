93c34a83e92129f2f8b3642d3834dc13a7f255ae
younboy123 <1hour.music1211@gmail.com>
2025-12-18 02:36:35 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("서버 정상 작동");
 });
 
-app.listen(3000, () => {
-  console.log("Server running");
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
 });
